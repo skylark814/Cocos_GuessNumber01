1e2c243bae8f4622ef50bd19a07904066cff30e6
skylark814 <skylark814@gmail.com>
2018-06-24 14:14:01 +0800

    version3 0624

--- src/guessNumber.js
+++ src/guessNumber.js
@@ -1,15 +1,39 @@
 var mainLayer = cc.Layer.extend({
-    sprite:null,
-    ctor:function () {
+    sprite: null,
+    dx : 10,
+    ctor: function () {   //建構式
 
         this._super();
 
+        var title = new cc.LabelTTF("猜數字遊戲", "", 48);
+        title.x = cc.winSize.width / 2;
+        title.y = cc.winSize.height * 7 / 8;
+        title.setColor(cc.color(255, 255, 0));
+        this.addChild(title, 0, "mytitle");
+
+        this.initLayout();
+        this.scheduleUpdate();  //update()
         return true;
+    },
+
+    initLayout:function(){
+
+    },
+
+
+    update: function () {
+        console.log("ok");
+        var title = this.getChildByName("mytitle");
+        if (title.x + title.width / 2 >= cc.winSize.width ||
+            title.x - title.width / 2 <= 0) {
+            this.dx *= -1;
+        }
+        title.x += this.dx;
     }
 });
 
 var mainScene = cc.Scene.extend({
-    onEnter:function () {
+    onEnter: function () {
         this._super();
         var layer = new mainLayer();
         this.addChild(layer);
