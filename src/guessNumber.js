566c321ac06822a2ee27bd04babdb13c57ec3763
skylark814 <skylark814@gmail.com>
2018-06-24 16:59:47 +0800

    version6 0624

--- src/guessNumber.js
+++ src/guessNumber.js
@@ -1,8 +1,12 @@
 var mainLayer = cc.Layer.extend({
     sprite: null,
     nums: new Array(10),
+    rects: new Array(10),
     back: null,
     enter: null,
+    input: null,
+    mesg: null,
+    guess: "",
     dx: 4,
     ctor: function () {
         this._super();
@@ -15,7 +19,7 @@ var mainLayer = cc.Layer.extend({
 
         this.initLayout();
 
-
+        this.setUpmymouse(this);
         this.scheduleUpdate();  // update()
 
         return true;
@@ -41,22 +45,66 @@ var mainLayer = cc.Layer.extend({
             this.nums[i].x = cc.winSize.width * px / 6;
             this.nums[i].y = cc.winSize.height * py / 8;
 
+            this.rects[i] = new cc.Rect(
+                this.nums[i].x - (this.nums[i].width / 2),
+                this.nums[i].y - (this.nums[i].height / 2),
+                this.nums[i].width,
+                this.nums[i].height
+            );
+
             this.addChild(this.nums[i]);
         }
 
         // enter key
         this.enter = new cc.Sprite(res.enter_png);
         this.enter.x = cc.winSize.width * 4 / 6;
-        this.enter.y = cc.winSize.height * 1 / 8;
+        this.enter.y = cc.winSize.height / 8;
         this.addChild(this.enter);
 
         // back key
         this.back = new cc.Sprite(res.back_png);
         this.back.x = cc.winSize.width * 2 / 6;
-        this.back.y = cc.winSize.height * 1 / 8;
+        this.back.y = cc.winSize.height / 8;
         this.addChild(this.back);
+        //input
+        this.input = new cc.LabelTTF("", "", 48);
+        this.input.x = cc.winSize.width * 3 / 6;
+        this.input.y = cc.winSize.height * 6 / 8;
+        this.addChild(this.input);
+        //mesg
+        this.mesg = new cc.LabelTTF("請輸入三位數", "", 48);
+        this.mesg.x = cc.winSize.width * 3 / 6;
+        this.mesg.y = cc.winSize.height * 5 / 8;
+        this.addChild(this.mesg);
 
 
+    },
+
+    setUpmymouse: function (layer) {
+        if ('mouse' in cc.sys.capabilities) {
+
+
+            var mouseListener = {
+                event: cc.EventListener.MOUSE,
+                onMouseDown: function (event) {
+                    var x = event.getLocationX();
+                    var y = event.getLocationY();
+
+                    var point = new cc.Point(x, y);
+
+                    for (i = 0; i < layer.rects.length; i++) {
+                        if (cc.rectContainsPoint(layer.rects[i], point)) {
+                            console.log("press " + i);
+                            layer.guess += i;
+                            layer.input.setString(layer.guess);
+                            break;
+                        }
+
+                    }
+                }
+            };
+            cc.eventManager.addListener(mouseListener, this);
+        }
     },
 
     update: function () {
